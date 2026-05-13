4bab5cb31f2564145aa4bfbedd660f207dc0412d
classroom6xunblocked-games <sdfgghhhhjfd@gmail.com>
2026-05-13 11:35:00 +0500

    Update footer.js

--- assest/js/footer.js
+++ assest/js/footer.js
@@ -278,7 +278,11 @@ function likeGames(game_id) {
         }
     }))
 }
-
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "vwjf1ycbo0");
 function formatNumberCompact(input) {
     input = parseInt(input);
     const p = input / 1000;
